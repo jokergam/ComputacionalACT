62007343ac69fb632148f5a171e06cc942d23fc4
Hugo Valenzuela Chaparro <hugo.vach@gmail.com>
2016-05-07 00:58:34 -0700

    añadiendo actividad 5

---
+++ Actividad5/pendulum3.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[34]:
+
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import numpy as np
+g=9.8
+
+def pend(y, t, b, c):
+        theta, omega = y
+        dydt = [omega, -b*omega - c*np.sin(theta)]
+        return dydt
+    
+#coeficiente de amortiguamiento    
+b = 0.5
+#longitud del péndulo
+l=1.0
+c = g/l
+#[angulo inicial, velocidad inicial]
+y0 = [np.pi-0.1, 0.0]
+t = np.linspace(0, 16, 101)
+
+#solucion
+sol = odeint(pend, y0, t, args=(b, c))
+        
+plt.plot(t, sol[:, 0], 'b', label='theta(t)')
+plt.plot(t, sol[:, 1], 'g', label='omega(t)')
+plt.legend(loc='best')
+plt.xlabel('t')
+plt.grid()
+plt.show()
+
+
+# In[ ]:
+
+
+
